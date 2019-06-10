9cfc00da851cb75049d60d31d8b95c85b1031014
Raghu Betina <raghu@firstdraft.com>
2019-06-10 05:37:30 +0000

    Only input user can modify input

--- app/controllers/inputs_controller.rb
+++ app/controllers/inputs_controller.rb
@@ -1,4 +1,14 @@
 class InputsController < ApplicationController
+  before_action :current_user_must_be_input_user, :only => [:edit_form, :update_row, :destroy_row]
+
+  def current_user_must_be_input_user
+    input = Input.find(params["id_to_display"] || params["prefill_with_id"] || params["id_to_modify"] || params["id_to_remove"])
+
+    unless current_user == input.user
+      redirect_to :back, :alert => "You are not authorized for that."
+    end
+  end
+
   def index
     @inputs = Input.all
 
