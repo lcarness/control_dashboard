a31db4c8f36c4b788def4b4d676e45afa28af2db
Raghu Betina <raghu@firstdraft.com>
2019-06-10 05:37:45 +0000

    Mappable address

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,11 @@
 class UsersController < ApplicationController
   def index
     @users = User.all
+    @location_hash = Gmaps4rails.build_markers(@users.where.not(:address_latitude => nil)) do |user, marker|
+      marker.lat user.address_latitude
+      marker.lng user.address_longitude
+      marker.infowindow "<h5><a href='/users/#{user.id}'>#{user.last_name}</a></h5><small>#{user.address_formatted_address}</small>"
+    end
 
     render("user_templates/index.html.erb")
   end
