ba3de152d8f5dde4cc8674465fa981924590e6c1
Raghu Betina <raghu@firstdraft.com>
2019-06-10 05:37:22 +0000

    Whitelist columns in Status for ActiveAdmin

--- app/admin/statuses.rb
+++ app/admin/statuses.rb
@@ -1,4 +1,6 @@
 ActiveAdmin.register Status do
+
+ permit_params :area_of_interest, :status, :action_plan
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
